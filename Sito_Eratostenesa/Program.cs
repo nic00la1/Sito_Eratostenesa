29bde6ec49dcf1f322a351cca2b9bf5e585858a4
Nicola Kaleta <nic00la.kaleta@gmail.com>
2025-11-14 10:45:02 +0100

    Dodaj pliki projektów.

---
+++ Sito_Eratostenesa/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace Sito_Eratostenesa
+{
+    public class Program
+    {
+        static bool[] tablica;
+
+        static void WypelnijTablice(int wielkosc)
+        {
+            tablica = new bool[wielkosc];
+            for(int i = 2; i < wielkosc; i++)
+            {
+                tablica[i] = true;
+            }
+        }
+
+        public static void Main(string[] args)
+        {
+            int n = 100;
+        }
+    }
+}
